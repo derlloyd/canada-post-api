808a4af51d3b21e88ca21854544f3df7f42c0188
Derek Lloyd <derlloyd@outlook.com>
2018-03-03 10:48:37 -0500

    functional

--- App.js
+++ App.js
@@ -1,167 +1,65 @@
-import React from "react";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
 import { StyleSheet, Text, View, WebView } from "react-native";
 
-export default class App extends React.Component {
-  onWebViewMessage(event) {
-    // post back reply as soon as possible to enable sending the next message
-          this.myWebView.postMessage(event.nativeEvent.data);
-    
-          let msgData;
-          try {
-              msgData = JSON.parse(event.nativeEvent.data);
-          }
-          catch(err) {
-              console.warn(err);
-              return;
-          }
-    
-          // invoke target function
-          const response = this[msgData.targetFunc].apply(this, [msgData]);
-          // trigger success callback
-          msgData.isSuccessfull = true;
-          msgData.args = [response];
-          this.myWebView.postMessage(JSON.stringify(msgData))
-    }
-  render() {
-    let html = `
-    <html>
-    <head>
-    <link rel="stylesheet" type="text/css" href="http://ws1.postescanada-canadapost.ca/css/addresscomplete-2.00.min.css?key=PN62-AK87-NW16-FE16" />
-        <script type="text/javascript" src="http://ws1.postescanada-canadapost.ca/js/addresscomplete-2.00.min.js?key=PN62-AK87-NW16-FE16"></script>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-        <style>
-        body {
+    this.state = {
+      text: "ReactNative WebView Sample"
+    };
 
-          flex-direction: row;
-          background: azure;
-          justify-content: center;
-          align-content: center;
-      }
-      button {
-          text-align: center;
-          align-self: center;
-          width:100%;
-          font-size: 150%;
-      }
-    label {
-        color: blue;
-        font-family: verdana;
-        font-size: 150%;
-    }
-    input {
-        color: blue;
-        font-family: verdana;
-        font-size: 150%;
-        width:100%;
-      }
-      p  {
-        color: red;
-        font-family: courier;
-        font-size: 150%;
-      }
-      .pca .pcatext {
-        font: 15px Verdana;
-        color: #4682b4;
-        width:100%;
+    this.onWebViewMessage = this.onWebViewMessage.bind(this);
   }
-    </style>
 
-    </head>
-    <body>
+  handleDataReceived(msgData) {
+    this.setState({
+      text2: `Message from web view ${msgData.data}`
+    });
+    msgData.isSuccessfull = true;
+    msgData.args = [msgData.data % 2 ? "green" : "red"];
+    this.myWebView.postMessage(JSON.stringify(msgData));
+  }
+
+  onWebViewMessage(event) {
     
-    <div>
-    <label for="street-address">Address</label>
-    <input id="street-address" type="text" placeholder="Start typing your address..." autofocus />
-    </div>
-
-<br />
-<br />
-<br />
-    <div class="input-line">
-    <label for="street-address"></label>
-    <input id="street-address1" type="text" placeholder="Address Line 1" />
-    </div>
-
-    <div class="input-line">
-    <label for="street-address2"></label>
-    <input id="street-address2" type="text" placeholder="Address Line 2" />
-    </div>
-
-    <div class="input-line">
-    <label for="city"></label>
-    <input id="city" type="text" placeholder="City" />
-    </div>
-
-    <div class="input-line">
-    <label for="state"></label>
-    <input id="state" type="text" placeholder="State/Province" />
-    </div>
-
-    <div class="input-line">
-    <label for="postcode"></label>
-    <input id="postcode" type="text" placeholder="Zip/Postcode" />
-    </div>
-
-    <div class="input-line">
-    <label for="country"></label>
-    <input id="country" type="text" placeholder="Country" />
-    </div>
-
-    <div id="container">
-    <button id="button" onclick="clickHandler();" >Send to RN</button>
-</div>
-
-    </body>
-    </html>
-    `;
-    let jsCode = `
-        
-        var fields = [
-          { element: "street-address", field: "Line1", mode: pca.fieldMode.DEFAULT },
-          { element: "street-address1", field: "Line1", mode: pca.fieldMode.POPULATE },
-          { element: "street-address2", field: "Line2", mode: pca.fieldMode.POPULATE },
-          { element: "city", field: "City", mode: pca.fieldMode.POPULATE },
-          { element: "state", field: "ProvinceName", mode: pca.fieldMode.POPULATE },
-          { element: "postcode", field: "PostalCode", mode: pca.fieldMode.POPULATE },
-          { element: "country", field: "CountryName", mode: pca.fieldMode.COUNTRY }
-        ]
-
-        options = {
-          key: "PN62-AK87-NW16-FE16",
-          autoSearch: true,
-          prompt: false,
-          setCursor: true,
-          setCountryByIP: true,
-          culture: "en-ca",
-          languagePreference: "eng"
-        }
-
-        control = new pca.Address(fields, options);
-
-        window.counter = 5;
-    function clickHandler() {
-		window.counter++;
-        window.webViewBridge.send('handleDataReceived', window.counter, function(res) {
-            window.document.getElementById("button").setAttribute("style", "background-color: " + res);
-        }, function(err) {
-			window.document.getElementById("container").setAttribute("style", "background-color: " + err);
-        });
+    let msgData;
+    try {
+      msgData = JSON.parse(event.nativeEvent.data);
+    } catch (err) {
+      console.warn(err);
+      return;
     }
-    `;
+    console.log("Message received from webview", msgData);
+    
+    switch (msgData.targetFunc) {
+      case "handleDataReceived":
+        this[msgData.targetFunc].apply(this, [msgData]);
+        break;
+    }
+  }
+
+  render() {
     return (
       <View style={styles.container}>
-        <WebView
-        ref={webview => { this.myWebView = webview; }}
-          source={{ html }}
-          // source={require('./form.html')}
-          style={styles.webView}
-          injectedJavaScript={jsCode}
-          // injectedJavaScript={require('./form.js')}
-          automaticallyAdjustContentInsets={true}
-          // onMessage={this.onWebViewMessage}
-          onMessage={(event)=> console.log("clicked",event.nativeEvent.data)}
-        />
-        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <Text style={styles.welcome}>{this.state.text}</Text>
+        <Text style={styles.welcome}>{this.state.text2}</Text>
+        <View style={styles.webViewContainer}>
+          <WebView
+            ref={webview => {
+              this.myWebView = webview;
+            }}
+            scrollEnabled={false}
+            source={require("./webview/index.html")}
+            onMessage={this.onWebViewMessage}
+          />
+        </View>
       </View>
     );
   }
@@ -170,14 +68,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 50
+    justifyContent: "center"
   },
-  webView: {
-    backgroundColor: "blue",
-    height: 350,
-    width: 350
+  welcome: {
+    flex: 1,
+    paddingTop: 20,
+    fontSize: 20,
+    textAlign: "center",
+    backgroundColor: "skyblue"
+  },
+  webViewContainer: {
+    flex: 4
   }
 });
--- form.js
+++ form.js
@@ -1,12 +1,13 @@
-document.querySelector("#myContent").style.backgroundColor = "red";
 var fields = [
   { element: "street-address", field: "Line1", mode: pca.fieldMode.DEFAULT },
+  { element: "street-address1", field: "Line1", mode: pca.fieldMode.POPULATE },
   { element: "street-address2", field: "Line2", mode: pca.fieldMode.POPULATE },
   { element: "city", field: "City", mode: pca.fieldMode.POPULATE },
   { element: "state", field: "ProvinceName", mode: pca.fieldMode.POPULATE },
   { element: "postcode", field: "PostalCode", mode: pca.fieldMode.POPULATE },
   { element: "country", field: "CountryName", mode: pca.fieldMode.COUNTRY }
 ];
+
 options = {
   key: "PN62-AK87-NW16-FE16",
   autoSearch: true,
@@ -16,4 +17,69 @@ options = {
   culture: "en-ca",
   languagePreference: "eng"
 };
+
 control = new pca.Address(fields, options);
+
+window.counter = 5;
+
+function clickHandler() {
+  window.counter++;
+  window.webViewBridge.send(
+    "handleDataReceived",
+    window.counter,
+    function(res) {
+      window.document
+        .getElementById("button")
+        .setAttribute("style", "background-color: " + res);
+    },
+    function(err) {
+      window.document
+        .getElementById("container")
+        .setAttribute("style", "background-color: " + err);
+    }
+  );
+}
+
+window.webViewBridge = {
+  /**
+   * send message to the React-Native WebView onMessage handler
+   * @param targetFunc - name of the function to invoke on the React-Native side
+   * @param data - data to pass
+   * @param success - success callback
+   * @param error - error callback
+   */
+  send: function(targetFunc, data, success, error) {
+
+    var msgObj = {
+      targetFunc: targetFunc,
+      data: data || {}
+    };
+
+    if (success || error) {
+      msgObj.msgId = guid();
+    }
+
+    var msg = JSON.stringify(msgObj);
+
+    promiseChain = promiseChain.then(function () {
+      return new Promise(function (resolve, reject) {
+        console.log("sending message " + msgObj.targetFunc);
+
+        if (msgObj.msgId) {
+          callbacks[msgObj.msgId] = {
+            onsuccess: success,
+            onerror: error
+          };
+        }
+
+        window.postMessage(msg);
+
+        resolve();
+      })
+    }).catch(function (e) {
+      console.error('rnBridge send failed ' + e.message);
+    });
+  },
+
+
+};
